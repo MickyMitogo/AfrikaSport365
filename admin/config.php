fdda88f3fea8beef6788be659b6d53143fba7d54
MickyMitogo <leonciofelipemitogoedjangobono@gmail.com>
2025-12-16 14:35:45 +0000

    actualizacion php

--- admin/config.php
+++ admin/config.php
@@ -2,7 +2,7 @@
 return [
     'admin_username' => 'admin',
     // Default password: ChangeMe123! (change immediately in production)
-    'admin_password_hash' => '$2y$10$QGQ7zY9bQWQ8H2pS9K0sneRjV0qQ0rJjU7E0cZK6lVtX6wX3QKXDa',
+    'admin_password_hash' => '$2y$10$UC1l1Xgwe2WBuJHOdUbV6.HcD3RNo99Z8fS50fGslOfFXPCNeRJ1O',
     'data_dir' => dirname(__DIR__) . DIRECTORY_SEPARATOR . 'data',
     'config_file' => dirname(__DIR__) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'config.json',
     'afcon_file' => dirname(__DIR__) . DIRECTORY_SEPARATOR . 'data' . DIRECTORY_SEPARATOR . 'afcon-data.json',
