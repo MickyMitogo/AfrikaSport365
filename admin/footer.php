ca5d419aa79cc0d5bb89bce7fe47cfd0d8e62ea6
MickyMitogo <leonciofelipemitogoedjangobono@gmail.com>
2025-12-20 11:04:04 +0000

    agregar bnews

--- admin/footer.php
+++ admin/footer.php
@@ -2,5 +2,6 @@
   <footer class="footer">© <?= date('Y') ?> AfrikaSport365</footer>
   <script src="/admin/assets/admin.js" defer></script>
   <script src="/admin/assets/afcon-admin.js" defer></script>
+  <script src="/admin/assets/breaking-news-admin.js" defer></script>
 </body>
 </html>
