b034ed0f1e7eab12779ed56d951c1942d695e2eb
MickyMitogo <leonciofelipemitogoedjangobono@gmail.com>
2025-12-20 13:36:34 +0000

    aver si va

--- admin/footer.php
+++ admin/footer.php
@@ -3,6 +3,5 @@
   <script src="/admin/assets/admin.js" defer></script>
   <script src="/admin/assets/afcon-admin.js" defer></script>
   <script src="/admin/assets/breaking-news-admin.js" defer></script>
-  <script src="/admin/assets/analisis-opinion-admin.v2.js" defer></script>
 </body>
 </html>
